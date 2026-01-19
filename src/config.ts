6e1572b58f1d4e0a12ce9e2d18ecb11b6c5ffb92
David Ovari <ovari.david.od@gmail.com>
2026-01-19 14:01:35 +0100

    Update config

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,6 @@
 export const siteConfig = {
   name: "Dávid Óvári",
-  title: "/ˈoːvaːri/\n\nI build applications for a living, delivering ideas right into your pocket",
+  title: "I build applications for a living, delivering ideas right into your pocket",
   description: "Portfolio website of Dávid Óvári",
   accentColor: "#4F93FF",
   social: {
@@ -11,7 +11,7 @@ export const siteConfig = {
   },
   aboutMe:
     "iOS Developer based in Hungary with hands-on experience building scalable applications used by millions. I specialize in writing clean, testable Swift code—leveraging UIKit & SwiftUI, RxSwift and Combine within modern architectures to ensure long-term maintainability. I enjoy working in collaborative teams to build high-quality features that balance performance and a great user experience!",
-  skills: ["Swift", "SwiftUI", "UIKit", "RxSwift", "Combine"],
+  skills: ["/ˈoːvaːri/", "Swift", "SwiftUI", "UIKit", "RxSwift", "Combine"],
   projects: [
     {
       name: "DrinkSpot: Daily Drink Tracker",
